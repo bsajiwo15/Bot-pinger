b6a9226de7f24db00a29a84e878f3024ca272d4c
Konicu <45599430+bsajiwo15@users.noreply.github.com>
2021-10-07 23:34:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const config = {
 
-    "prefix": "!", //Add your prefix here
+    "prefix": "", //Add your prefix here
 
     "ownerOnly": false, //If set to true, only bot owner can control Pinger. If set to false, everyone can use Pinger.
 
     "ownerID": "", //Id of bot owner for "ownerOnly" function.
 
-    "channelName": "ping-test", //Name of channel, where will be all pings sent.
+    "channelName": "", //Name of channel, where will be all pings sent.
 
     "pingInterval": "1000", //Time in ms (miliseconds). 1000 recommended - If 1000ms (1s), bot will send ping every 1000ms (1s).
 
-    "token": "ODk1NjgxMjIyMDI0NDk1MTQ1.YV8GJA.rLtbga8PR7Wr4buq6ObGipQ1Xvs" //Bot token from discord app.
+    "token": "" //Bot token from discord app.
 
 }
 
